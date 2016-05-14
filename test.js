a8d906ce91acb2a2ae7c2dfc38dcbeac4524fb62
Frank Anderson <frob@249517.no-reply.drupal.org>
2016-05-14 17:59:40 -0500

    added testing for randomFrom

--- test.js
+++ test.js
@@ -5,11 +5,14 @@ const rc = new RandomCrap();
 
 let sum = 0;
 const iterations = 10000;
+const tenSet = [1, , 12,3,4,5,6,7,8,9,10];
+console.log(rc.randomFrom(tenSet));
 
 console.log('control: ' + ((sum + iterations)/2));
 
 for (let i = 0; i < iterations; i ++) {
   sum += rc.simpleRandomInt(0, iterations);
+  // rc.setBias(rc.getBias() + i);
 }
 
 console.log('actual: ' + sum/iterations);
