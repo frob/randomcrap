3ed7e11f5fe6a040e68a264db52d0555303e8a4c
Frank Anderson <frob@249517.no-reply.drupal.org>
2016-05-14 17:05:49 -0500

    added a good random bias to the random in generator

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 'use strict';
 
 class RandomCrap {
+  constructor() {
+    this.biasAdjuster = 0.289;
+  }
+  const biasAdjuster;
   /**
    * Returns a random integer from min (inclusive) to max (inclusive).
    */
   simpleRandomInt(min, max) {
     const mathRandom = Math.random();
     const likelyAdjuster = Math.floor(mathRandom * ((10 + 1) - 1)) + 1;
-    const biasAdjuster = 0.289;
     let random = parseFloat('0.' + (Math.random() * mathRandom * (new Date().getTime())));
 
     // This algorythm has no basis in math and it naturally biases low,
     // therefore it will need some adjustment from time to time. This has been
     // tested and with its defaults comes out to be random.
-    if (random < biasAdjuster && Math.round(mathRandom * 100) % likelyAdjuster > 0) {
-      random += biasAdjuster;
+    if (random < this.biasAdjuster && Math.round(mathRandom * 100) % likelyAdjuster > 0) {
+      random += this.biasAdjuster;
     }
 
     return Math.floor(random * ((max + 1) - min)) + min;
