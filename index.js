239a3a2f1e466274789bb2843dad850e89ee8c43
Frank Anderson <frob@249517.no-reply.drupal.org>
2016-05-14 17:59:06 -0500

    added random from set

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
 'use strict';
 
 class RandomCrap {
-  constructor() {
-    this.biasAdjuster = 0.289;
+  /**
+   * @param biasAdjuster float
+   *   Allows setting a high or low bias for random generation.
+   */
+  constructor (biasAdjuster) {
+    // This is used in the random number generation to allow for high or low
+    // biasing. This can be used in a game to adjust the fun or difficulty.
+    this.biasAdjuster = (typeof biasAdjuster === 'undefined') ? 0.289 : biasAdjuster;
   }
-  const biasAdjuster;
+
+  /**
+   * @param bias float
+   *   Allows setting a high or low bias for random generation. Setting nothing
+   *   will reset to neutral bias.
+   */
+  setBias (bias) {
+    this.biasAdjuster = (typeof bias === 'undefined') ? 0.289 : bias;
+  }
+
+  getBias () {
+    return this.biasAdjuster;
+  }
+
+  randomFrom(set, name) {
+    console.log(set);
+    // if (typeof name === 'undefined') {
+      let fromSet = set;
+    // }
+    // else {
+    //   let fromSet = [];
+    //   // @TODO: all for sets to be saved.
+    // }
+
+    const length = fromSet.length;
+    const keys = fromSet.keys();
+    const indexMap = [...fromSet.keys()];
+    const indexGet = this.simpleRandomInt(0, length - 1);
+    const randomElement = fromSet[indexMap[indexGet]];
+
+    set.splice(fromSet[indexMap[indexGet]], 1);
+
+    return randomElement;
+  }
+
   /**
    * Returns a random integer from min (inclusive) to max (inclusive).
    */
-  simpleRandomInt(min, max) {
+  simpleRandomInt (min, max) {
     const mathRandom = Math.random();
     const likelyAdjuster = Math.floor(mathRandom * ((10 + 1) - 1)) + 1;
     let random = parseFloat('0.' + (Math.random() * mathRandom * (new Date().getTime())));
